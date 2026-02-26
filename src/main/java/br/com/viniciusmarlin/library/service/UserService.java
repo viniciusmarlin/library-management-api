9efb57fc843e3c93ebad0bfed983d99e7ba9da35
Vinicius Santos <viniciusmarlinpessoal@gmail.com>
2026-02-26 20:50:13 -0300

    docs: add explanatory comments in user service

--- src/main/java/br/com/viniciusmarlin/library/service/UserService.java
+++ src/main/java/br/com/viniciusmarlin/library/service/UserService.java
@@ -9,36 +9,44 @@ import at.favre.lib.crypto.bcrypt.BCrypt;
 @Service
 public class UserService {
 
+    // Injeção de dependência do IUserRepository
     private final IUserRepository userRepository;
 
+    // Construtor para injetar o IUserRepository
     public UserService(IUserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
+    // Método para registrar um novo usuário
     public UserModel register(UserDTO.RegisterUserDTO dto) {
 
+        // Verificar se o email já está registrado
         UserModel user = new UserModel();
         user.setName(dto.name());
         user.setEmail(dto.email());
 
+        // Hash da senha usando BCrypt
         String hashedPassword = BCrypt.withDefaults()
                 .hashToString(12, dto.password().toCharArray());
 
-        user.setPassword(hashedPassword);
-
-        return userRepository.save(user);
+        user.setPassword(hashedPassword); // Armazenar a senha encriptada
+        return userRepository.save(user); // Salvar o usuário no banco de dados
     }
 
+    // Método para login de usuário
     public UserModel login(String email, String password) {
+
+        // Buscar o usuário pelo email
         var user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
 
-        var result = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
 
+        var result = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword()); // Verificar a senha usando BCrypt
+
+        // Se a senha não for válida, lançar uma exceção
         if(!result.verified) {
             throw new RuntimeException("Senha incorreta");
         }
-
-        return user;
+        return user; // Se a senha for válida, retornar o usuário
     }
 }
